c61b70f79bd6f70e3e37eb3ecabbb54f6eb908fa
AbhishekAggarawal <abhishekmonu2000@gmail.com>
2024-11-08 21:52:54 +0530

    deploy

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+
+
+export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  return (
+    <div className="max-w-5xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+      Organize, Plan, and Collaborate – All in One Unified Workspace. <br /> <br /> Welcome to <span className="underline">NoteSphere</span>
+      </h1> <br />
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+      Seamlessly manage tasks, take notes, and sync ideas across your personal or team projects,
+      just the way you need.
+      </h3> <br />
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter SyncBoard
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get SyncBoard free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+    </div>
+  )
+}
